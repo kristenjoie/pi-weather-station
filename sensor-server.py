1c678af1851a4fba9e6d731611549ecb2008106d
Kristen Joie <kjoie.dev@gmail.com>
2022-07-04 20:45:27 +0200

    Fix sensor settings

--- sensor-server.py
+++ sensor-server.py
@@ -37,7 +37,16 @@ elif args.sensor_type == 'bme':
     import threading
 
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
-    
+    sensor.set_humidity_oversample(bme680.OS_2X)
+    sensor.set_pressure_oversample(bme680.OS_4X)
+    sensor.set_temperature_oversample(bme680.OS_8X)
+    sensor.set_filter(bme680.FILTER_SIZE_3)
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+    sensor.set_gas_heater_temperature(320)
+    sensor.set_gas_heater_duration(150)
+    sensor.select_gas_heater_profile(0)
+
     global gas, gas_baseline, hum, air_quality_score
 
     gas = 250000
